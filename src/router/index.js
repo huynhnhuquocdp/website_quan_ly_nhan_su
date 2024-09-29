8a8ab2e57be7e25fc4ba7372564a266dc663ed3e
Quốc Sang <sang0882k3@gmail.com>
2024-09-30 00:18:37 +0700

    tieuchi

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,34 @@
 import { createRouter, createWebHistory } from "vue-router"; // cài vue-router: npm install vue-router@next --save
 
 const routes = [
-    {
-        path : '/',
-        component: ()=>import('../layout/wrapper/index.vue')
-    },
+  {
+    path: "/",
+    component: () => import("../layout/wrapper/index.vue"),
+  },
 
-    // <!-- VIET -->
+  // <!-- VIET -->
 
-    // <!-- END_VIET -->
+  // <!-- END_VIET -->
 
-    // <!-- SANG -->
+  // <!-- SANG -->
+  {
+    path: "/admin/tieu-chi-kpi",
+    component: () => import("../components/Admin/TieuChiKpi/index.vue"),
+  },
+  // <!-- END_SANG -->
 
-    // <!-- END_SANG -->
+  // <!-- QUY -->
 
-    // <!-- QUY -->
+  // <!-- END_QUY -->
 
-    // <!-- END_QUY -->
+  // <!-- DUC -->
 
-    // <!-- DUC -->
-
-    // <!-- END_DUC -->
-]
+  // <!-- END_DUC -->
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: routes
-})
+  history: createWebHistory(),
+  routes: routes,
+});
 
-export default router
+export default router;
